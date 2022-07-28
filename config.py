b63ed433682139d7dff11c018e9ad87249af46dd
cbarraza93 <102003692+cbarraza93@users.noreply.github.com>
2022-07-27 22:11:14 -0500

    update

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
-# Enter your API key
-gkey = "AIzaSyDe98bUbAXtwcFbVnMUSbv_9p_7jwTZ36s"
+# Dependencies
+api_key = "oEMPPqbOnsEKYDKmjGquXtDbGxJNLALA"
+
+conn = 'mongodb+srv://cbarraza:Gramercy1@cluster0.q26pfay.mongodb.net/Cluster0'
+
+api_key2 = "AIzaSyCzvH7z6yIfVKWK6McYetJwnE1HN7F8vpA"
